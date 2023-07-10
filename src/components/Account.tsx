a90f8f5dc0c383e65736a6838755045018213410
artemkolodko <temakolodko@gmail.com>
2023-07-10 13:34:13 +0100

    Temp hide export private key button

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {Box} from "grommet";
 import {Typography} from "antd";
 import {observer} from "mobx-react";
@@ -8,24 +8,24 @@ const { Text } = Typography
 
 export const AccountInfo = observer(() => {
   const { authStore } = useStores()
-  const [isPKVisible, setPKVisible] = useState(false)
+  // const [isPKVisible, setPKVisible] = useState(false)
 
   return <Box gap={'16px'}>
     <Box>
       <Text type={'secondary'} style={{ fontSize: 'small' }}>Address</Text>
       <Text copyable={true} style={{ fontSize: 'small' }}>{authStore && authStore.userAccount && authStore.userAccount.address}</Text>
-      {authStore?.userAccount &&
-          <Box direction={'row'}>
-              <Box>
-                  <Text underline={true} style={{ fontSize: 'small' }} onClick={() => setPKVisible(!isPKVisible)}>
-                    {isPKVisible ? 'Hide' : 'Show'} private key
-                  </Text>
-                {(isPKVisible) &&
-                    <Text style={{ fontSize: 'small' }}>{authStore.userAccount.privateKey}</Text>
-                }
-              </Box>
-          </Box>
-      }
+      {/*{authStore?.userAccount &&*/}
+      {/*    <Box direction={'row'}>*/}
+      {/*        <Box>*/}
+      {/*            <Text underline={true} style={{ fontSize: 'small' }} onClick={() => setPKVisible(!isPKVisible)}>*/}
+      {/*              {isPKVisible ? 'Hide' : 'Show'} private key*/}
+      {/*            </Text>*/}
+      {/*          {(isPKVisible) &&*/}
+      {/*              <Text style={{ fontSize: 'small' }}>{authStore.userAccount.privateKey}</Text>*/}
+      {/*          }*/}
+      {/*        </Box>*/}
+      {/*    </Box>*/}
+      {/*}*/}
     </Box>
     <Box>
       <Text type={'secondary'} style={{ fontSize: 'small' }}>Balance</Text>
